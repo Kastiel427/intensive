d3a1cec7d83c8f7a8cf47b59f3ca98105c1bf6f1
Gutyrchik.A.V <kastiel427.nexus@gmail.com>
2016-07-18 16:22:50 +0300

    Добавил скрипт создания таблиц

---
+++ Database/CreateTables.sql
@@ -0,0 +1,42 @@
+use GutyrchikIntensive
+go
+create table Projects
+(
+	ProjectID int primary key not null,
+	ProjectName varchar(30) not null
+)
+go
+create table Tasks
+(
+	TaskID int primary key not null,
+	TaskName varchar(30) not null,
+	Specification varchar(300) not null,
+)
+go
+create table [Tasks in projects]
+(
+	ProjectID int not null,
+	TaskID int not null,
+	Closed varchar(3) not null,
+	Deadline datetime,
+
+	constraint PK_TaskInProjects primary key (ProjectID, TaskID),
+	constraint FK_TasksInProjects_Project foreign key (ProjectID) references Projects(ProjectID),
+	constraint FK_TasksInProjects_Tasks foreign key (TaskID) references Tasks(TaskID)
+)
+go
+create table Tags
+(
+	TagID int primary key not null,
+	TagName varchar(30) unique not null 
+)
+go
+create table [Tags in tasks]
+(
+	TaskID int not null,
+	TagID int not null,
+
+	constraint PK_TagInTasks primary key(TaskID, TagID),
+	constraint FK_TagsInTasks_Tasks foreign key (TaskID) references Tasks(TaskID),
+	constraint FK_TagsInTasks_Tags foreign key (TagID) references Tags(TagID)
+)
