859d4a0fd201cc8d45adf0605bf8c52714f8f454
Gutyrchik.A.V <kastiel427.nexus@gmail.com>
2016-07-19 16:55:47 +0300

    Исправил ошибки в скриптах к бд

---
+++ application/Task1/Task1/Task1/Startup1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Task1.Startup1))]
+
+namespace Task1
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+                return context.Response.WriteAsync("<h2>Привет мир!</h2>");
+            });
+        }
+    }
+}
---
+++ application/Task1/Task1_Test/Task1_Test/Startup1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Task1_Test.Startup1))]
+
+namespace Task1_Test
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+                return context.Response.WriteAsync("<h2>Привет мир!</h2>");
+            });
+        }
+    }
+}
